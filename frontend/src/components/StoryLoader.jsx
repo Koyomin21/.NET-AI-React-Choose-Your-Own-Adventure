d670248374bec2a1580abc3004c00f2bff2ca8d2
Anuar <anuar12102001@gmail.com>
2025-07-13 12:20:49 +0500

    [add] router. [add] StoryGame, StoryLoader components

--- frontend/src/components/StoryLoader.jsx
+++ frontend/src/components/StoryLoader.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import {useParams, useNavigate} from 'react'
 import axios from 'axios'
 import LoadingStatus from "./LoadingStatus";
+import StoryGame from "./StoryGame";
 
 const API_BASE_URL = "/api"
 
@@ -56,7 +57,7 @@ function StoryLoader() {
 
     if(story) {
         return <div className="story-loader">
-
+            <StoryGame story={story} onNewStory={createNewStory} />
         </div>
     }
 }
